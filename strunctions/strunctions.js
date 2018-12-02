b8153dd1cac854d696b1e6dcf208f20bed5765ed
Ken Snow <k.snowjr@gmail.com>
2018-12-01 19:00:32 -0700

    Newsies complete

---
+++ strunctions/strunctions.js
@@ -0,0 +1,34 @@
+var str = "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.";
+/*
+
+function replaceMostCommonWordWith(testStr,replacementWord){
+    var cleanTestStr = testStr.replace(/\./g, '');
+    cleanTestStr = cleanTestStr.toLowerCase();
+    var words = cleanTestStr.split(" ");
+    var matches = [];
+    var x = 0;
+    //test each word has been added to array
+
+    for (var i = 0; i < words.length; i++){
+        for (var n = 0; n < words.length; n++){
+            //test to ensure its not testing the same word
+            if (i != n){
+                //create object with key, and increment value if match
+                if (words[i] === words[n]){
+                    console.log("match on " + words[i]);
+}
+                    
+                }
+
+            }
+            
+        }
+        
+    }
+
+}
+
+replaceMostCommonWordWith(str,"bob");
+
+
+*/
