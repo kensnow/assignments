b0f6f670abd65167bd319c6bba5718ca84ef9c93
Ken Snow <k.snowjr@gmail.com>
2018-12-09 21:39:35 -0700

    mario-pest-control complete

--- mario-pest-control/script.js
+++ mario-pest-control/script.js
@@ -1,78 +1,135 @@
-/*
 
-Mario Pest Control
+var updateButton = document.querySelector("#update-button")
+var submitButton = document.querySelector("#submit-button")
+var pestButtons = document.querySelectorAll(".pestButton")
+var form = document.querySelector("#summary")
+var doc = document.body;
+coinSound = new Audio("coin.wav")
+//baddies object.
+//could have impreoved structure here by adding
+//the baddie as a nested object
+//ie. baddies = [{ goomba = {value:5,}}]
+var baddies = [{
+    name:"Goomba",
+    id:"goomba",
+    value:5,
+    getKills: function (){
+        return document.querySelector("#goomba").value       
+    },
+},{
+    name:"Bob-omb",
+    id:"bobomb",
+    value:7,
+    getKills: function (){
+        return document.querySelector("#bobomb").value       
+    },
+},{
+    name:"Cheep Cheep",
+    id:"cheepCheep",
+    value:11,
+    getKills: function (){
+        return document.querySelector("#cheepCheep").value       
+    },
+}]
 
-Princess Peach has a huge infestation of Goombas, Bob-ombs, and Cheep-cheeps, and has commissioned Mario to take care of her pest problem. Mario did the job, but he is having trouble keep track of how many he has caught and has asked you to build him a website that keeps track of how many baddies he caught for Princess Peach so he can send her a bill.
-
-Pricing:
-
-Goombas: 5 Coins
-Bob-ombs: 7 Coins
-Cheep-cheeps: 11 Coins
-Baddies Caught:
-
-The number of baddies caught will be entered into an input field
-
-Total Price
-
-This will be dynamicly depending on how many baddies were caught.
-
-Project Requirements
-The website must contain the following:
-
-A list displaying each baddie by type. Each list item must:
-Have baddie image
-Have baddie name
-Show price of that baddie
-Have an input box to enter the total caught of that type
-A total price at the bottom that adds sums the total cost of baddies caught
-(must use Javascript for this math - no hard coded values)
-
-A footer showing:
-
-Mario's email address
-Mario's company website url
-Mario's physical address
-A large title at the top of the page entitled "Mario Pest Control"
-Passing Criteria: Visual Inspection by Instructor
-Student demonstrated a solid understanding with the following levels of the skills tree:
-
-Static Websites, Level 1
-CSS Selectors
-Colors in CSS
-Fonts in CSS
-Using external CSS stylesheets and JavaScript files
-Deployment, Level 1
-Opening an HTML document in browser
-Extra Credit
-Build Mario Pest Control website to be responsive, meaning it looks great when shrunk down to the screen size of a smart phone.
-
-
-
-*/
-
-var goomba = 5;
-var bobomb = 7;
-var cheepCheep = 11;
-
-var total = document.querySelector("total-display")
-
-var inputArr = document.querySelectorAll(".input")
-//establish handlers for buttons, total, and input fields
-
-for(i=0; i< inputArr.length; i++){
-    inputArr[i].addEventListener("input",function(e){
-        updateTotal(e);
-        
+//store pest buttons in event listeners
+for (i = 0; i < pestButtons.length; i++){
+    pestButtons[i].addEventListener("click",function(e){
+        //increment kill count by one on click
+        id = e.target.name;
+        valueDisp = parseInt(document.querySelector("#"+id).value)
+        // console.log(valueDisp)
+        document.querySelector("#"+id).value = valueDisp + 1
+        coinSound.play();
     })
 }
 
-function getTotalEntered() {
-
+function getTotalCoins(inputArray){
+    let newSum = 0;
+    for(i=0; i< inputArray.length; i++){
+        let killVal = baddies[i].value;
+        let totalKills = parseInt(baddies[i].getKills());
+        newSum += killVal * totalKills;
+    }
+    return newSum
 }
-function buttonFunctionality() {
 
-}
-function updateTotal(e){
+function findKillValue(id, baddies){
+    //this function iterates though the baddies object to locate id,
+    //then returns value of that baddie.
+    let value = 0;
+    for (i = 0; i < baddies.length; i++){
+        if (baddies[i].id === id){
+            //add found id value
+            value += baddies[i].value;
+
+        }
+    }
     
+    return value
 }
+
+function updateTotal(newValue){
+    //get current 'total' placeholder & replace
+    var total = document.querySelector("#total-display");
+    total.innerHTML = ("Total: " + newValue);
+
+}
+
+function submitBill(){
+    //hide elements 3-7 on form
+    let cNodes = [3,5,7]
+
+    for (i = 0; i < cNodes.length; i++){
+        form.childNodes[cNodes[i]].style.visibility = "hidden";
+        form.childNodes[cNodes[i]].style.height = "15px";
+        form.style.padding = "20px";
+        textBox = cNodes[i] - 1;
+        form.childNodes[textBox].textContent = (baddies[i].name + ": " + baddies[i].value + " coins x " + baddies[i].getKills() + " kills = " + baddies[i].value * baddies[i].getKills())
+    }
+    
+    form.children.totalBox.children.update.style.display = "none"
+    form.children.totalBox.children.submit.style.display = "none"
+    //replace with invoice data
+    title = document.querySelector("#summary-title")
+    title.innerText = "Invoice:"
+
+
+
+}
+
+
+///////// Event Listeners
+
+//fix zeroing out bug.  
+//Form would reset anytime entered was pressed when filling in a number
+
+
+form.addEventListener("submit",function(e){
+
+    e.preventDefault();
+
+    return false;
+})
+
+//update total button and enter will be the same
+updateButton.addEventListener("click", function(e){
+    e.preventDefault();
+    //on click, need to read all the values in, and update total 
+    updateTotal(getTotalCoins(baddies));
+    }
+)
+//enter key listener
+doc.addEventListener("keyup", function(e){
+    e.preventDefault();
+    if(e.keyCode === 13){
+        updateTotal(getTotalCoins(baddies));
+    } 
+}
+)
+
+submitButton.addEventListener("click",function(e){
+    updateTotal(getTotalCoins(baddies));
+    submitBill();
+
+})
