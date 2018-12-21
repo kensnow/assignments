c7aa8814a0d732201e2754aeff35ba483506c4f5
Ken Snow <k.snowjr@gmail.com>
2018-12-21 10:21:49 -0700

    todo complete, css zen complete

--- async-todo-axios/script.js
+++ async-todo-axios/script.js
@@ -1,44 +1,4 @@
-/*
-Axios Todo Project
-28 AUGUST 2017
-You will be creating a Todo list.
 
-Use the totally rad todo API created by the legendary Bob. Here are the docs for it.
-
-For the requirements below, you'll be building a frontend site that pulls your list of todos down from the API to display them, and allows the user to perform other CRUD methods on them as well.
-
-For example, when the user adds a new todo, it will also POST that todo to the database using the provided API. This way, once the change is made, it's made permanently. When you refresh the page, since it's pulling from the data on the API, it should pull the current list of todos.
-
-You will do the parts of this assignment in steps. You're encouraged to have someone else look at your code between each step. This will help you to become comfortable talking about code, and might open you up to other ways of doing things. Any suggestions given to you by fellow students should be considered, but don't feel obligated to apply the suggestions you get.
-
-Requirements
-Use the checklist in Trello to mark off each step before proceeding to the next.
-
-Part 1 - GET
-The user can see their current list of todos.
-Todos show up as soon as the page loads.
-If a todo item is complete, it should have a strikethrough line on it
-Images should be displayed as images if there are any
-Part 2 - POST
-The user can add new todos to their list. The new item should be posted to the todo API so a future reload of the page will still display that new todo item.
-A user should be able to give the item a price.
-A user should be able to give the item a description.
-A user should be able to attach an imgUrl to the item
-Part 3 - PUT Part 1
-Each todo will have a checkbox where it can be marked complete or incomplete
-Checking the checkbox should update the database
-Part 4 - DELETE
-A user will be able to delete todos (this is different from marking a todo as "completed")
-Each todo should be rendered with a button marked "X" or "Delete" that when clicked, will delete the Todo
-Part 5 - PUT Part 2
-Each Todo will have an "edit" button.
-When clicked, the info will change to input boxes that are autofilled with the old Todo data
-A user can change the value of these inputs
-When the "edit" button is clicked, it will change to a "save" button.
-When "save" is clicked, the form will disapear, and the new values will be displayed.
-On save, the todo will be edited in the database
-Read through the "using id" section in the API documentation to learn how to delete items using the item's unique id.
-*/
 const addButton = document.querySelector("#addTodid")
 const inputs = document.querySelectorAll(".input")
 const url = "https://api.vschool.io/kensnow/todo/"
@@ -88,10 +48,17 @@ function updateList(data){
     return todoArr;
 }
 
-function appendTodo(toDo){
+//take todo object & render. if it is an edit,refrence object to be edited
+function appendTodo(toDo, editDiv){
+    let todoDiv = {}
+    if (editDiv){
+        todoDiv = editDiv
+    } else {
+        todoDiv = document.createElement("div")
+    }
     let list = document.querySelector("#the-list")
     //create elements
-    let todoDiv = document.createElement("div")
+    
     let title = document.createElement("h4")
     let desc = document.createElement("p")
     let value = document.createElement("p")
@@ -107,7 +74,9 @@ function appendTodo(toDo){
     let statusNode = document.createTextNode(`completed: ${toDo.completed}`)
     let deleteButtonNode = document.createTextNode("Delete")
     let editButtonNode = document.createTextNode("Edit")
+
     // add event listeners
+
     deleteButton.addEventListener("click", e => {
         axios.delete(url + toDo.id)
             .then(todoDiv.remove())
@@ -117,11 +86,14 @@ function appendTodo(toDo){
     })
     
     editButton.addEventListener("click", e => {
-        //get ph text
-        console.log()
+        
+        //get placeholder text
+ 
         let titleText = title.textContent
         let descText = desc.textContent
         let valueText = value.value
+        let updateTodo = {}
+        
         //remove items to be replaced
         editButton.remove()
         title.remove()
@@ -151,27 +123,45 @@ function appendTodo(toDo){
 
         todoDiv.style.display = "grid"
         value.style.width = "50px"
-        console.log(submitButton)
         
         todoDiv.appendChild(submitButton)
         todoDiv.appendChild(title)
         todoDiv.appendChild(desc)
         todoDiv.appendChild(value)
-
+        
         //create event listener on submit, to send back data to api
         submitButton.addEventListener("click", function(e){
             //title,description,price,img,completed = false,id
-            let updateTodo = new Todo(title,desc,value)
-            console.log("url: " + url + " todo.id: " + toDo.id +  " updatetodo: " + updateTodo)
+            // input = this.parent
+            let children = this.parentElement.childNodes
+                
+                if (children[3].value && children[3].value !== titleText){
+                    updateTodo.title = children[3].value       
+                } 
+                if (children[4].value && children[4].value !== desc){
+                    updateTodo.description = children[4].value
+                }
+                if (children[5].value && children[5].value !== value){
+                    updateTodo.price = children[5].value
+                } 
+                
+                updateTodo.completed = toDo.completed;
+                title.remove()
+                desc.remove()
+                value.remove()
+                submitButton.remove()   
+                
+            // let updateTodo = new Todo(title,desc,value)
+ 
             axios.put(url + toDo.id, updateTodo)
                 .then(response => {
-                    appendTodo(response.data)
+                    appendTodo(response.data, todoDiv)
 
                 })
         })
     })
 
-
+    
     todoDiv.addEventListener("click", function(e){
         //update completed status to toggle true/false
         //check to ensure user is not trying to click on buttons
@@ -239,8 +229,10 @@ function appendTodo(toDo){
    
 
 }
+
 const todoArr = getData()
 
+//get inputs from submit click from array
 function getTodoInput(inputs){
     let title, description,img = ""; 
     inputs.forEach(function(element){
@@ -275,7 +267,7 @@ function getTodoInput(inputs){
             img
 
         }
-        console.log("title, price, description, img" + title, price, description, img)
+        
         return todo
     }
 }
