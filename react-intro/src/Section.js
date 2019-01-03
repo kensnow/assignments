555ce37d6d2051f407c2ff11cefc43883d5bd5c5
Ken Snow <k.snowjr@gmail.com>
2019-01-02 17:03:41 -0700

    react-intro

---
+++ react-intro/src/Section.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section = () => {
+    return (
+        <section>
+            <p>More stuff and more</p>
+        </section>
+    )
+}
+
+export default Section
