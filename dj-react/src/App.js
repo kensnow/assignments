58b9d13e79ffcb54e418d6dcaa894f877bed5efa
Ken Snow <k.snowjr@gmail.com>
2019-01-07 08:59:14 -0700

    button-1 working, need to pass props to squares

--- dj-react/src/App.js
+++ dj-react/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react"
 import Squares from "./Squares"
 import ButtonBox from "./ButtonBox"
-
+import styles from "./styles.css"
 
 class App extends Component {
     render(){
         return(
             <div>
-                <Squares/>
+                {/* <Squares/> */}
                 <ButtonBox/>
             </div>   
         )
--- dj-react/src/Button1.js
+++ dj-react/src/Button1.js
@@ -1,33 +1,48 @@
 import React, {Component} from "react"
 import Squares from "./Squares"
 
+const currentStyles = [
+    {
+    id:"square-1",
+    style:"backgroundColor:white"
+    },{
+    id:"square-2",
+    style:"backgroundColor:white"
+    },{
+    id:"square-3",
+    style:"backgroundColor:white"
+    },{
+    id:"square-4",
+    style:"backgroundColor:white"
+    }
+]
+
 class Button1 extends Component {
     constructor(){
         super();
         this.state = {
-          currentStyles:[
-            {
-            id:"square-1",
-            style:[]
-            },{
-            id:"square-2",
-            style:[]
-            },{
-            id:"square-3",
-            style:[]
-            },{
-            id:"square-4",
-            style:[]
-            }]
+            backgroundColor:"white"
         } //Must be an object!
         this.handleClick = this.handleClick.bind(this)
       }
     handleClick(e){
-        let updateStyles = currentStyles.map(style => style ==="backgroundColor:black" ? "backgroundColor:white":"backgroundColor:black" )
-        
-        this.setState(previousState => ({
-            
-        })
+        if (this.state.backgroundColor === "black"){
+            this.setState({backgroundColor:"white"})
+         } else {
+        this.setState({backgroundColor:"black"})
+        }
+    }
+    render(){
+        return (
+            <div className="container">
+                <div style={this.state} className="party-box"></div>
+                <div style={this.state} className="party-box"></div>
+                <div style={this.state} className="party-box"></div>
+                <div style={this.state} className="party-box"></div>
+                <button onClick={this.handleClick}>Dj React!</button>
+             </div>
+        )
+       
     }
     
 }
