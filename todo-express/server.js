3a19bde1f3c08ef1eb2aa06fda33ce9d7d465c8b
Ken Snow <k.snowjr@gmail.com>
2019-01-23 13:50:48 -0700

    todo-express complete

--- todo-express/server.js
+++ todo-express/server.js
@@ -8,20 +8,37 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/trails", (req,res) => {
-    res.send(Database.find())
+app.get("/trails", (req, res) => {
 
+    res.send(Database.find())
+    
 })
 
-app.post("/trails",(req,res) => {
+app.get("/trails/:id?", (req, res) => {
+   
+    let id = req.params.id
+  
+    res.status(200).send(Database.find(id))
+ 
+})
+
+
+app.post("/trails", (req, res) => {
     //insert save database funtion
     res.status(200).send(Database.save(req.body))
 })
 
-app.delete("/trails/:id",(req, res) => {
+app.delete("/trails/:id", (req, res) => {
     let id = req.params.id
-    Database.findByIdAndRemove(id) ? res.status(204).send("Delete Completed") : console.log("problem deleting " + id)
+    Database.findByIdAndRemove(id) ? res.status(204).send("Delete Completed") : res.send("problem deleting " + id)
 
+})
+
+app.put("/trails/:id", (req, res) => {
+    let id = req.params.id
+    let payload = req.body
+    let response = Database.findByIdAndUpdate(id, payload)
+    res.status(200).send(response)
 })
 
 app.listen(process.env.PORT, () => console.log("server is listening!"))
--- todo-express/trails.js
+++ todo-express/trails.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid');
 
-const Trails = function (trailInfo) {
+const Trail = function (trailInfo) {
     this._id = uuid();
     this.name = trailInfo.name;
     this.area = trailInfo.area;
@@ -16,25 +16,41 @@ const Database = function(){
     this.Trails = []
 }  //this builds the database as an array of the trail objects
 
-Database.prototype.find = function(){
-    return this.Trails
+Database.prototype.find = function(trailId){
+    
+    if (trailId){
+        let foundTrail = this.Trails.find(trail => {return trail._id === trailId})
+        return foundTrail
+    } else {
+        return this.Trails
+    }
+    
+   
 }
 
 Database.prototype.save = function(trailInfo){
-    const newTrail = new Trails(trailInfo)
+    const newTrail = new Trail(trailInfo)
     this.Trails.push(newTrail)
     return newTrail
 }
 
 Database.prototype.findByIdAndRemove = function(trailId){
-    const foundTrail = this.Trails.find((trail) => {
-        return (trail._id === trailId)
-    })
-    
+    let foundTrail = this.Trails.find((trail) => {return (trail._id === trailId)})
+
     if(!foundTrail) return false
-    const index = this.Trails.indexOf(foundTrail)
+    let index = this.Trails.indexOf(foundTrail)
     this.Trails.splice(index, 1)
     return true
+}
+
+Database.prototype.findByIdAndUpdate = function(trailId, updates){
+    let foundTrail = this.Trails.find(trail => {return trail._id === trailId})
+
+    if(!foundTrail) return false
+    let index = this.Trails.indexOf(foundTrail)
+    let updatedTrail = {...foundTrail,...updates}
+    this.Trails.splice(index, 1, updatedTrail)
+    return(updatedTrail)
 }
 
 module.exports = new Database()
